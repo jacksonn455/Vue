aa3169552424e2589a7d2d894dd0cccde06454a6
Jackson Felipe Magnabosco <jackson.magnabosco_B@pampa.compasso>
2020-03-05 14:09:43 -0300

    first commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,64 @@
+<!-- Single file template que tmb é um módulo-->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1> <!-- interpolação, realiza um databind que vai da fonte de dados para a view -->
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<!-- Script - comportamento e o dado-->
+<script>
+export default {
+  name: 'app',
+  data () { // fornece os dados que o template precisa, sempre retorna um objeto javascript
+    return {
+      msg: 'Bem vindo ao mundo de Calopsita'
+    }
+  }
+}
+</script>
+
+
+<!-- Style - estilo do template-->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
